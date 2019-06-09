941b00ac5c7a1aab98038686c9450f3458241c85
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-06-09 20:18:44 +0200

    fix base_url

--- lib/controllers/application_controller.rb
+++ lib/controllers/application_controller.rb
@@ -2,7 +2,7 @@ module PokerArena
   class ApplicationController < Sinatra::Base
     def base_url
       @base_url ||=
-        "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+        "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
     end
 
     def json_params
