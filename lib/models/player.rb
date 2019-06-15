ad0e7ba3aa630b846afa14e0dad0fed06b0a5030
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-06-16 00:03:02 +0200

    add Cash to Player

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -2,11 +2,11 @@ module PokerArena
   class Player
     MAX_CARDS = 2
 
-    attr_reader :token, :cards, :pseudo
-
-    def initialize(pseudo:)
+    attr_reader :token, :cards, :pseudo, :stack, :cash
+    def initialize(pseudo:, cash: Cash.new)
       @cards = []
       @pseudo = pseudo
+      @cash = cash
     end
 
     def receive_card(card)
